d3d5ef511bb6c5cbabbc01ec43936bc200f3584e
Optimizer-Prime <stu3cla@protonmail.com>
2021-03-07 11:18:10 -0500

    added navbar and sidebar

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish")
+)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
+    created_on = models.DateField(auto_now_add=True)
+    last_updated = models.DateField(auto_now=True)
+    body = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    # sort database results by created_on date, descending by default
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class BlogListView(ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
